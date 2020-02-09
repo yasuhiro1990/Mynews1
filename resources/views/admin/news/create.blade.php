2a044a7ddb5faf5f3c961ac78051887943557b03
Yasuhiro <felnand9torres@gmail.com>
2020-02-09 03:01:17 +0000

    mynews

---
+++ resources/views/admin/news/create.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>MyNews</title>
+    </head>
+    <body>
+        @extends('layouts.admin')
+        
+        @section('title','ニュースの新規作成')
+        
+        @section('content')
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 mx-auto">
+                        <h2>ニュースの新規作成</h2>
+                    </div>
+                </div>
+            </div>
+        @endsection
+    </body>
+</html>
